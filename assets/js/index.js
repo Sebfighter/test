15fe53e415c41cb79786eaaab0e3f4fbd37368dd
Sebfighter <sebastiankevin2000@gmail.com>
2022-07-18 22:04:06 -0400

    commit 3

--- assets/js/index.js
+++ assets/js/index.js
@@ -14,30 +14,48 @@ function stickyNav() {
 
   // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
   function myFunction() {
-      if (window.pageYOffset >= sticky) {
-          navbar.classList.add("sticky")
-      } else {
-          navbar.classList.remove("sticky");
-      }
+    if (window.pageYOffset >= sticky) {
+      navbar.classList.add("sticky")
+    } else {
+      navbar.classList.remove("sticky");
+    }
   }
 }
 // Oculta Card-Text
-function toggleSection(){
-    $('#destacados').click(function() {
-        $('.card-text').toggleClass("invisible");
-    });
+function toggleSection() {
+  $('#destacados').click(function () {
+    $('.card-text').toggleClass("invisible");
+  });
 }
-
 // Función onClick para enviar correo de contacto
 function btnEnviar() {
   alert("El correo fue enviado correctamente...");
+}
+function liveAlert() {
+  const alertPlaceholder = document.getElementById('liveAlert')
+  const alert = (message, type) => {
+    const wrapper = document.createElement('div')
+    wrapper.innerHTML = [
+      `<div class="alert alert-${type} alert-dismissible" role="alert">`,
+      `   <div>${message}</div>`,
+      '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+      '</div>'
+    ].join('')
+    alertPlaceholder.append(wrapper)
+  }
+  const alertTrigger = document.getElementById('liveAlertBtn')
+  if (alertTrigger) {
+    alertTrigger.addEventListener('click', () => {
+      alert('El correo fue enviado correctamente!', 'warning')
+    })
+  }
 }
 
 // Habilita tooltip bootstrap
 function enableTooltips() {
   var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
   var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-      return new bootstrap.Tooltip(tooltipTriggerEl)
+    return new bootstrap.Tooltip(tooltipTriggerEl)
   })
 }
 
@@ -54,6 +72,8 @@ toggleSection();
 stickyNav();
 
 enableTooltips();
+
+liveAlert();
 
 
 /////////////////////////////////////////////////////////////////////////////////
